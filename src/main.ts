a4ed7605b84f63b4a48baa5f383664ba6abbd40d
Florian Keller <github@floriankeller.de>
2018-11-25 14:27:25 +0100

    chore: Remove mac and windows target, add small icon

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import {BrowserWindow, app} from 'electron';
 import * as path from 'path';
-import {app, BrowserWindow} from 'electron';
 import {platform} from './utils';
 
 const APP_PATH = app.getAppPath();
 const BASE_URL = 'https://my.1password.com';
-const ICON_PATH = path.join(APP_PATH, 'resources', 'icons', 'icon@128x128.png');
+const ICON_PATH = path.join(APP_PATH, 'resources', 'icon@128x128.png');
 
 let mainWindow: BrowserWindow | null = null;
 
@@ -17,11 +17,11 @@ const createWindow = () => {
     height: 750,
     icon: ICON_PATH,
     title: '1Password',
-    width: 1000,
     webPreferences: {
       nodeIntegration: false,
       nodeIntegrationInWorker: false,
     },
+    width: 1000,
   });
 
   mainWindow.webContents.on('will-navigate', url => console.log(url));
